15cd523119ef01bde0eaf54023f8c9d2b5203a1e
Artur08 <artbaghdasaryan0808@gmail.com>
2022-03-10 00:33:42 +0400

    v12

--- update.cmd
+++ update.cmd
@@ -4,10 +4,3 @@ git pull origin main
 cd Game/base.io_Data/ 
 unzip sharedassets0.assets.zip 
 unzip sharedassets1.assets.zip 
-cd ../../Database/
-cmd start "BaseIODatabase.exe"
-cd ../Server/
-cmd start "Survio Server.exe"
-cd ../Game/
-cmd start "base.io.exe"
-cd ../
